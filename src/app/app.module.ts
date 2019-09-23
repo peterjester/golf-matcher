a27279d713cf740e83196f1249d5c6fb2b2e494a
Peter <peter.j.jester@gmail.com>
2019-09-22 20:59:51 -0400

    Base authenitcation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbAuthFirebaseUIModule } from '@firebaseui/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbAuthFirebaseUIModule.forRoot({
+      apiKey: "AIzaSyBdDGhasKTCRmyGJe79iY-V5ZH-cnL-kBg",
+      authDomain: "golf-matcher-cf449.firebaseapp.com",
+      databaseURL: "https://golf-matcher-cf449.firebaseio.com",
+      projectId: "golf-matcher-cf449",
+      storageBucket: "",
+      messagingSenderId: "935607959094",
+      appId: "1:935607959094:web:d1d9ecda2c6d4e7489e297"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
