8f5b35dc523fe930110213d9956390a6b6d1440b
Bill Ganley <wag945@gmail.com>
2019-10-05 18:36:38 -0400

    Add players component and player service.

---
+++ src/app/players/player.ts
@@ -0,0 +1,10 @@
+export class Player {
+    id: number;
+    name: string;
+    nickname: string;
+    email: string;
+    phone: string;
+    age: number;
+    handicap: number;
+    league: string;
+}
---
+++ src/mock-players.ts
@@ -0,0 +1,45 @@
+// import { Player } from './players/player';
+import {Player} from './app/players/player';
+
+export const Players: Player[] = [
+    { 
+        id: 1, 
+        name: 'William Ganley', 
+        nickname: "Bill", 
+        email: 'wag945@psu.edu',
+        phone: '1234567890',
+        age: 39,
+        handicap: 14,
+        league: 'one'
+    },
+    { 
+        id: 2, 
+        name: 'Peter Jester', 
+        nickname: "Pete", 
+        email: 'peter.jester@gmail.com',
+        phone: '1234567890',
+        age: 25,
+        handicap: 18,
+        league: 'one'
+    },
+    { 
+        id: 3,
+        name: 'Craig Roland',
+        nickname: 'Craig',
+        email: 'craig.roland@gmail.com',
+        phone: '2345678901',
+        age: 25,
+        handicap: 4,
+        league: 'one'
+    },
+    {
+        id: 4,
+        name: 'Daniel Castellucci',
+        nickname: 'Dan',
+        email: 'dan.castellucci@gmail.com',
+        phone: '3456789012',
+        age: 25,
+        handicap: 15,
+        league: 'one'
+    }
+];
