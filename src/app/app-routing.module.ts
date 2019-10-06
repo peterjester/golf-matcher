01bb9bcfea42e200ec9b567794a2c36de6ff5d4b
Bill Ganley <wag945@gmail.com>
2019-10-06 18:51:29 -0400

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PlayersComponent } from './players/players.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { PlayerEditComponent }  from './player-edit/player-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'players', component: PlayersComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'edit/:id', component: PlayerEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
