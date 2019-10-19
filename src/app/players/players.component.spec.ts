54046e73cdda614093f8d9e269ddb1a1b425067c
Bill Ganley <wag945@gmail.com>
2019-10-19 17:47:25 -0400

    PlayersComponent unit tests

--- src/app/players/players.component.spec.ts
+++ src/app/players/players.component.spec.ts
@@ -3,15 +3,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlayersComponent } from './players.component';
 import { Players } from 'src/mock-players';
 import { Player } from 'src/app/players/player';
+import { PlayerService } from '../player.service';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 describe('PlayersComponent', () => {
   let component: PlayersComponent;
   let fixture: ComponentFixture<PlayersComponent>;
   let player: Player;
+  let playerService: PlayerService;
+  let renderer2: Renderer2;
+  let db: AngularFirestore;
+
+  // beforeEach(() => { 
+  //   db = new AngularFirestore(null,null,null,null,null,null,null);
+  //   playerService = new PlayerService(db);  
+  //   component = new PlayersComponent(playerService,renderer2); 
+  // });
+
 
   it ('Test PlayersComponent name',() => {
     expect(fixture).name === "PlayersComponent";
-  });
+   });
 
   it('Test PlayersComponent creation', () => {
     expect(PlayersComponent).toBeTruthy();
@@ -21,5 +34,7 @@ describe('PlayersComponent', () => {
     expect(Players).length > 0;
   });
 
+  it('Test PlayersComponent onSelect()',() => { 
+  });
 
 });
